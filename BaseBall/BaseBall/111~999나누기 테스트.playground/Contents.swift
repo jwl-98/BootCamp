666f64b19f9c0a5f151a07b9e1cc36f2885c3769
이진욱KR <apple_komputer@icloud.com>
2024-11-04 15:32:10 +0900

    Feat : "LV1. readLine 추가 while문을 이용해 111~999까지의 랜덤값중 0이 들어간 세자리수를 제외해서 새로운 변수에 할당 동일값여부 로직추가"
    body :  인풋 숫자는 엔터를 입력할 필요없이 바로 세자리수를 입력하도록 설정했습니다.
    랜덤숫자의 생성의 경우 클로저 내부에 0이 들어간 세자리수를 제외하는 로직을 넣어놨습니다.
    
     0이 들어간 세자리수가 나오게되면 반복문은 계속 반복됩니다.
    ran2라는 변수에 할당이 되는순간 반복문을 벗어나 ran2를 반환합니다.
    
    if let 바인딩을 통해서 옵셔널스트링 형태의 인풋값을 Int형으로 변경해주고
    동일값여부를 판단하는 로직을
    추가했습니다  
    Related to: 문자열을 입력하면 "정수를 입력하세요" 가 아닌 "숫자가일치하지 않는다"의 문장이 출력
    문자열이 그대로 출력됨

---
+++ BaseBall/BaseBall/111~999나누기 테스트.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+
+for i in 111...999 {
+    
+    if (i%10 != 0) {
+        print(i)
+    }
+}
+//확인완료
--- BaseBall/BaseBall/main.swift
+++ BaseBall/BaseBall/main.swift
@@ -7,5 +7,48 @@
 
 import Foundation
 
-print("Hello, World!")
+//LV 1
+//1에서 9까지의 서로 다른 임의의 수 3개를 정하고 맞추는 게임입니다
+//정답은 랜덤으로 만듭니다.(1에서 9까지의 서로 다른 임의의 수 3자리)
+
+let inputString = readLine()
+let inputnum: Int?
+
+
+//랜덤값 추출로직
+let randomNum = {
+    let ran1 = Int.random(in: 111...999)
+    var ran2 = 0
+    
+    repeat {
+        if ran1%10 != 0 {
+            ran2 = ran1
+            break
+        }
+    }while(ran2>0)
+
+    return ran2
+}()
+
+
+//옵셔널 바인딩과 동일값 여부 로직
+if let inputnum = inputString {
+    print("랜덤숫자 : \(randomNum)")
+    print("입력한 수는 \(inputnum)")
+    
+    if Int(inputnum) == randomNum {
+        print("숫자가 일치한다.")
+    }else{
+        print("숫자가일치하지 않는다.")
+    }
+}else{
+    print("정수를입력하세요.")
+}
+
+
+
+
+
+
+
 
