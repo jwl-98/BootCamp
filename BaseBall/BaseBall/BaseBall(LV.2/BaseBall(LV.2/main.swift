ba4957f06dcfc7eb3ccb18ff4178212757c40106
이진욱KR <apple_komputer@icloud.com>
2024-11-04 21:30:16 +0900

    fix : 랜덤숫자 생성시 0으로 생성되는 경우 발견 , refactor : 클래스 내부에 넣고 각 기능별로 메서드 생성
    getRadomNumber() 메서드 내부 변수 ran2의 값을 0으로 설정 해놨기 때문에
    if에서 false가 나오게 되면 while문의 조건에서도 false가 나오게 되어 반복문이 실행이 되지 않고  변수 ran2가 0으로 빠져나오는 경우를 발견했습니다.
    변수가 초기화 되지 않았으면 계속 반복하라는 의도로 조건을 새롭게 변경했습니다.
    
     BaseBallGame 클래스 내부에 집어넣고 외부에서 인스턴스를 생성해서 게임 시작할수 있도록 변경했습니다.
    생성자를 통해서 readLine()함수를 통한 입력값을 초기화 할수 있도록 변경 했고,  동시에 랜덤숫자도 생성이됩니다.
    start() 메서드를 실행시키면 입력값이 전달되는 동시에 결과가 나옵니다.

---
+++ BaseBall/BaseBall/BaseBall(LV.2/BaseBall(LV.2/main.swift
@@ -0,0 +1,71 @@
+//
+//  main.swift
+//  BaseBall(LV.2
+//
+//  Created by 진욱의 Macintosh on 11/4/24.
+//
+
+import Foundation
+
+class BaseBallGame {
+    
+    var inputString: String?
+    
+    var inputNum = 0
+    var randomNumber = 0
+    
+    init(input: String?){
+        self.inputString = input
+        randomNumber = getRadomNumber()
+    }
+    
+    func start() {
+        checkUserInput(inputString: inputString)
+    }
+    
+    //랜덤값 추출로직
+    func getRadomNumber() -> Int {
+        let ran1 = Int.random(in: 111...999)
+        var ran2 = 0
+        
+        repeat {
+            //랜덤값의 나머지가 0이 아니라면 == 세자리수중 0이 없는 숫자들이면
+            if (ran1%10 != 0) && ((ran1/10)%10 != 0) {
+                ran2 = ran1
+                //조건문이 true라면 반복문을 벗어남.
+                break
+            }
+            //ran2 값이 0인경우 -> 초기화 되지 않은경우
+        }while(ran2 == 0)
+        
+        return ran2
+    }
+    
+    
+    //옵셔널 바인딩과 동일값 여부 로직
+    func checkUserInput(inputString: String?) {
+        
+        if let inString = inputString {
+            if let numString = Int(inString){
+                inputNum = numString
+                print("랜덤숫자 : \(randomNumber)")
+                print("입력한 수는 \(inputNum)")
+            } else {
+                print("정수를 입력하세요")
+            }
+        }
+        
+        if randomNumber == inputNum {
+            print("숫자가 동일하다.")
+        } else {
+            print("동일하지 않다.")
+        }
+    }
+    
+}
+
+
+let Stringinput = readLine()
+let game = BaseBallGame(input: Stringinput)
+game.start()
+
