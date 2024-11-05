e472a8347569ca2a1a3212eaef2bd09516730ed5
이진욱KR <apple_komputer@icloud.com>
2024-11-05 17:21:42 +0900

    feat : LV.2 조건에 맞지 않는 수가 입력되는 경우 에러메세지 출력되는 기능 추가
    -LV.2 조건 문자열X, 세자리수 초과 X , 세자리수중 0이 껴있으면 안됨.
    -입력과 동시에 입력값을 검증하기위해서 checkUserInput()함수 내부에 readLine()을 구현했습니다.
    -또한 LV.2조건에 일치한다면 입력값을 setUserNumberToArray(input: ) 함수의 파라미터로 전달해 배열이 생성되도록 했습니다.
    -사용자의 입력값은 지속적인 확인이 필요해 BaseBallGameLogic()내부의 repeat - whil()문에 넣어 함수를 지속적으로 실행되게 했습니다.

--- BaseBall/LV.2/main.swift
+++ BaseBall/LV.2/main.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class BaseBallGame {
     
-    var inputString: String? //입력값을 저장하기위한 변수
+   // var inputString: String? //입력값을 저장하기위한 변수
     var inputNum = 0 //입력값을 저장하기위한 변수
     
     var randomNumber = 0 //랜덤숫자를 저장하기위한 변수
@@ -17,15 +17,11 @@ class BaseBallGame {
     var userNumberArray:[Int] = [] //세자리 수를 배열에 담기위한 배열
     var randomArray:[Int] = []
     
-    init(input: String?){
-        self.inputString = input
-    }
     
+
     func start() {
         print(#function)
-        checkUserInput(inputString: inputString)
         randomArray = getRadomNumberArray()
-        userNumberArray = getUserNumberArray()
         BaseBallGameLogic()
     }
     
@@ -55,63 +51,88 @@ class BaseBallGame {
         return randomArray.reversed()
     }
     
-    
-    
-    func getUserNumberArray() -> [Int] {
+    //유저의 입력값 검증
+    func checkUserInput() -> Bool {
+        print("숫자를 입력하세요 : ")
+        let Stringinput = readLine()
+        
+        
         print(#function)
-        var n = inputNum //입력된 수를 저장하는 변수
+        if let inString = Stringinput { //입력값이 존재한다면.
+            if let numString = Int(inString){
+                inputNum = numString
+                if inputNum < 1000 && ((inputNum%10 != 0) && ((inputNum/10)%10 != 0)) {
+                    //세자리 수를 초과하지 않고, 0이 껴있는 세자리 수가 아니라면 true반환
+                    print("입력한 수는 \(inputNum)")
+                    //입력된 수를 배열로 변경해주기 위한 함수 호출
+                    setUserNumberToArray(input: inputNum)
+                    return true
+                }
+            }
+        }
+        // 조건 이외의 입력값이 입력되면 에러메세지 출력과 false반환
+        print("-------------------")
+        print("올바르지 않은 입력값입니다")
+        print("-------------------")
+        
+        return false
+    }
+    
+    func setUserNumberToArray(input: Int) {
+        print(#function)
+        var n = input //입력된 수를 저장하는 변수
+        userNumberArray = [] //다시호출되면 배열 초기화
         while(n > 0) {
-            
             userNumberArray += [n%10]
             n /= 10
         }
-        
-        return userNumberArray.reversed()
+        userNumberArray = userNumberArray.reversed()
     }
     
     
-    //유저의 입력값 검증
-    func checkUserInput(inputString: String?) {
-        print(#function)
-        if let inString = inputString { //입력값이 존재한다면.
-            if let numString = Int(inString){
-                inputNum = numString
-                print("입력한 수는 \(inputNum)")
-            } else {
-                print("정수를 입력하세요")
-            }
-        }
-    }
+
     
     
     func BaseBallGameLogic() {
-        print(#function)
-        var countStrike = 0 //스트라이크 수 카운트를 저장하기위한 변수
-        var countBall = 0 //볼 수 카운트를 저장하기위한 변수
+        //print(#function)
         
-        
-        //랜덤숫자의 인덱스와 요소를 튜플의 형태로 추출
-        for (Index, Element) in randomArray.enumerated() {
-        
-            //유저배열의 인덱스 넘버를 기반으로 요소를 추출
-            //랜덤숫자의 요소와 동일하다면 스트라이크 카운트 1증가
-            if userNumberArray[Index] == Element  {
-                countStrike += 1
-                if countStrike == 3 { //3스트라이크의 경우 - 정답인경우
+        repeat {
+           
+            //카운트 수가 누적되지 않게 반복문에 들어가면 카운트 초기화
+            var countStrike = 0 //스트라이크 수 카운트를 저장하기위한 변수
+            var countBall = 0 //볼 수 카운트를 저장하기위한 변수
+            
+            //유저의 인풋이 조건에 맞다면 true을 반환하고, 게임 시작.
+            if checkUserInput() == true {
+                //랜덤숫자의 인덱스와 요소를 튜플의 형태로 추출
+                for (Index, Element) in randomArray.enumerated() {
+                    print(userNumberArray)
+                    
+                    //유저배열의 인덱스 넘버를 기반으로 요소를 추출
+                    //랜덤숫자의 요소와 동일하다면 스트라이크 카운트 1증가
+                    if userNumberArray[Index] == Element  {
+                        countStrike += 1
+                    }else if userNumberArray.contains(Element) {
+                        countBall += 1
+                    }
+                }
+                
+                if countStrike != 0 || countBall != 0 {
+                    print("\(countStrike)스트라이크")
+                    print("\(countBall)볼")
+                }
+                
+                if countStrike == 0 && countBall == 0 {
+                    print("Nothing")
+                }else if countStrike == 3 {
                     print("정답")
                     break
                 }
-            }else if userNumberArray.contains(Element) {
-                countBall += 1
             }
-
-        }
-        print("\(countStrike)스트라이크")
-        print("\(countBall)볼")
+        }while (true)
     }
 }
 
-let Stringinput = readLine()
-let game = BaseBallGame(input: Stringinput)
+let game = BaseBallGame()
 game.start()
 
