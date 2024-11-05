26bc812740c781632699b6e62695936ca306dbf4
이진욱KR <apple_komputer@icloud.com>
2024-11-05 14:46:20 +0900

    feat : LV2 기능중 숫자비교 로직에 따라 결과를 출력하는 기능 구현
    -각 인덱스 넘버를 비교하고 조건이 true면 스트라이크 카운드를 증가시키는 로직을 구현했습니다.
    -enumerated() 메서드를사용해 랜덤배열의 인덱스와 요소를 추출해서 유저넘버와 비교합니다.
    -countStrike 변수가 3이 된다는건 숫자가 일치한다는것이니 break를 통해 반복문을 벗어납니다.

--- BaseBall/LV.2/main.swift
+++ BaseBall/LV.2/main.swift
@@ -9,61 +9,107 @@ import Foundation
 
 class BaseBallGame {
     
-    var inputString: String?
+    var inputString: String? //입력값을 저장하기위한 변수
+    var inputNum = 0 //입력값을 저장하기위한 변수
     
-    var inputNum = 0
-    var randomNumber = 0
+    var randomNumber = 0 //랜덤숫자를 저장하기위한 변수
+    
+    var userNumberArray:[Int] = [] //세자리 수를 배열에 담기위한 배열
+    var randomArray:[Int] = []
     
     init(input: String?){
         self.inputString = input
-        randomNumber = getRadomNumber()
     }
     
     func start() {
+        print(#function)
         checkUserInput(inputString: inputString)
+        randomArray = getRadomNumberArray()
+        userNumberArray = getUserNumberArray()
+        BaseBallGameLogic()
     }
     
     //랜덤값 추출로직
-    func getRadomNumber() -> Int {
-        let ran1 = Int.random(in: 111...999)
+    func getRadomNumberArray() -> [Int] {
+        print(#function)
+        randomNumber = Int.random(in: 111...999)
         var ran2 = 0
-        
         repeat {
-            //랜덤값의 나머지가 0이 아니라면 == 세자리수중 0이 없는 숫자들이면
-            if (ran1%10 != 0) && ((ran1/10)%10 != 0) {
-                ran2 = ran1
+            // 세자리수중 0이 없는 숫자들 추출하기
+            if (randomNumber%10 != 0) && ((randomNumber/10)%10 != 0) {
+                ran2 = randomNumber
+                while(ran2 > 0) {
+                    randomArray += [ran2%10]
+                    ran2 /= 10
+                }
                 //조건문이 true라면 반복문을 벗어남.
                 break
+            } else { //랜덤숫자중 0이 껴있으면 숫자를 다시생성
+                randomNumber = Int.random(in: 111...999)
+                continue
             }
             //ran2 값이 0인경우 -> 초기화 되지 않은경우
         }while(ran2 == 0)
         
-        return ran2
+        print("랜덤숫자 : \(randomNumber)")
+        return randomArray.reversed()
     }
     
     
-    //옵셔널 바인딩과 동일값 여부 로직
-    func checkUserInput(inputString: String?) {
+    
+    func getUserNumberArray() -> [Int] {
+        print(#function)
+        var n = inputNum //입력된 수를 저장하는 변수
+        while(n > 0) {
+            
+            userNumberArray += [n%10]
+            n /= 10
+        }
         
-        if let inString = inputString {
+        return userNumberArray.reversed()
+    }
+    
+    
+    //유저의 입력값 검증
+    func checkUserInput(inputString: String?) {
+        print(#function)
+        if let inString = inputString { //입력값이 존재한다면.
             if let numString = Int(inString){
                 inputNum = numString
-                print("랜덤숫자 : \(randomNumber)")
                 print("입력한 수는 \(inputNum)")
             } else {
                 print("정수를 입력하세요")
             }
         }
-        
-        if randomNumber == inputNum {
-            print("숫자가 동일하다.")
-        } else {
-            print("동일하지 않다.2")
-        }
     }
     
-}
+    
+    func BaseBallGameLogic() {
+        print(#function)
+        var countStrike = 0 //스트라이크 수 카운트를 저장하기위한 변수
+        var countBall = 0 //볼 수 카운트를 저장하기위한 변수
+        
+        
+        //랜덤숫자의 인덱스와 요소를 튜플의 형태로 추출
+        for (Index, Element) in randomArray.enumerated() {
+        
+            //유저배열의 인덱스 넘버를 기반으로 요소를 추출
+            //랜덤숫자의 요소와 동일하다면 스트라이크 카운트 1증가
+            if userNumberArray[Index] == Element  {
+                countStrike += 1
+                if countStrike == 3 { //3스트라이크의 경우 - 정답인경우
+                    print("정답")
+                    break
+                }
+            }else if userNumberArray.contains(Element) {
+                countBall += 1
+            }
 
+        }
+        print("\(countStrike)스트라이크")
+        print("\(countBall)볼")
+    }
+}
 
 let Stringinput = readLine()
 let game = BaseBallGame(input: Stringinput)
