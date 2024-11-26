eb5a63f788a2787bc2f3f7f8cdf79a200ee8bdc0
이재영 <ldc1000@naver.com>
2024-11-26 17:32:26 +0900

    [Feat] : 레이아웃 설정
    - titleLabel: UILabel (서비스명 표시 라벨)
    - bellButton: UIButton (호출버튼)

--- nbc-popcon/nbc-popcon/Sources/Views/HeaderView.swift
+++ nbc-popcon/nbc-popcon/Sources/Views/HeaderView.swift
@@ -6,7 +6,74 @@
 //
 
 import UIKit
+import SnapKit
 
 class HeaderView: UIView {
     
+    private let titleLabel: UILabel = {
+        let titleLabel = UILabel()
+        
+        titleLabel.font = ThemeManager.shared.fonts.h2
+        titleLabel.text = "POPCON"
+        titleLabel.textColor = .white
+
+        return titleLabel
+    }()
+    
+    private let bellButton: UIButton = {
+        let callButton = UIButton()
+        
+        let callImage = UIImage(systemName: "bell.fill")
+        callButton.setImage(callImage, for: .normal)
+        callButton.contentMode = .scaleAspectFit
+        callButton.backgroundColor = .clear
+        callButton.tintColor = .white
+        
+        return callButton
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+        setUpUI()
+    }
+}
+
+
+//MARK: - HeaderView (setUpUI)
+extension HeaderView {
+    
+    private func setUpUI() {
+        setUpTitleLabel()
+        setUpBellButton()
+        self.backgroundColor = .red
+    }
+    
+    private func setUpTitleLabel() {
+        self.addSubview(titleLabel)
+        
+        titleLabel.snp.makeConstraints { label in
+            label.centerX.centerY.equalToSuperview()
+        }
+    }
+    
+    private func setUpBellButton() {
+        self.addSubview(bellButton)
+        
+        bellButton.snp.makeConstraints { button in
+            button.centerY.equalTo(titleLabel)
+            button.trailing.equalToSuperview().inset(ThemeManager.shared.numbers.padding)
+            button.height.width.equalTo(32)
+        }
+        
+        bellButton.imageView?.snp.makeConstraints { imageView in
+            imageView.size.centerX.centerY.equalToSuperview()
+        }
+    }
 }
