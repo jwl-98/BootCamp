fb0ee39a41942827007904aae8dc24bdf6d5b5de
이재영 <ldc1000@naver.com>
2024-11-27 15:19:06 +0900

    [Fix] 메뉴아이템 구조 수정 및 데이터 추가
    - 카테고리 열거형 수정
    - 메뉴 아이템 구조 수정

--- nbc-popcon/nbc-popcon/Sources/Models/Category.swift
+++ nbc-popcon/nbc-popcon/Sources/Models/Category.swift
@@ -7,18 +7,31 @@
 
 import Foundation
 
-enum Category: Int, CaseIterable {
+enum Category: String, CaseIterable {
     
-    case communication = 0
-    case tool = 1
-    case health = 2
+    case communication = "통신"
+    case tool = "사물 및 도구"
+    case health = "건강"
+    case others = "기타"
+//    
+//    var kor: String {
+//        switch self {
+//        case .communication: return "통신"
+//        case .tool: return "사물 및 도구"
+//        case .health: return "건강"
+//        }
+//    }
     
-    var kor: String {
-        switch self {
-        case .communication: return "통신"
-        case .tool: return "사물 및 도구"
-        case .health: return "건강"
+    init(rawValue: String) {
+        switch rawValue {
+        case "통신":
+            self = .communication
+        case "사물 및 도구":
+            self = .tool
+        case "건강":
+            self = .health
+        default:
+            self = .others
         }
     }
-    
 }
