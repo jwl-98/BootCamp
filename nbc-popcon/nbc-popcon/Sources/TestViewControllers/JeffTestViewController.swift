d58e2264254702c14033b7d544b5a4a12d5f19c8
jeff-0324 <jeffap324@gmail.com>
2024-11-27 11:35:24 +0900

    [Feat] tableView Cell 구현 완료

--- nbc-popcon/nbc-popcon/Sources/TestViewControllers/JeffTestViewController.swift
+++ nbc-popcon/nbc-popcon/Sources/TestViewControllers/JeffTestViewController.swift
@@ -1,33 +1,7 @@
 //
-//  JeffTestViewController.swift
+//  Untitled.swift
 //  nbc-popcon
 //
-//  Created by jae hoon lee on 11/25/24.
+//  Created by jae hoon lee on 11/26/24.
 //
-
-import UIKit
-import SnapKit
-
-class JeffTestViewController: UIViewController {
-    
-    let sample: UIView = CartView()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        self.view.addSubview(sample)
-        
-        configUI()
-    }
-
-    func configUI() {
-        sample.snp.makeConstraints {
-            $0.leading.equalTo(view.snp.leading).inset(10)
-            $0.trailing.equalTo(view.snp.trailing).inset(10)
-            $0.bottom.equalTo(view.snp.bottom).inset(100)
-            $0.height.equalTo(571)
-        }
-    }
-}
-
 
