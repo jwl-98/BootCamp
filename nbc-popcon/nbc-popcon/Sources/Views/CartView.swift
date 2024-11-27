82235eed6d362414d750644f0775640c1f09018a
jeff-0324 <jeffap324@gmail.com>
2024-11-27 15:10:34 +0900

    [Update] background 생성 및 지정된 폰트, 컬러, 패딩 적용, 세부적인 레이아웃 변경

--- nbc-popcon/nbc-popcon/Sources/Views/CartView.swift
+++ nbc-popcon/nbc-popcon/Sources/Views/CartView.swift
@@ -14,11 +14,20 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     var totalCount: Int = 7
     
     //MARK: - 컴포넌트 생성
+    
+    // Background View
+    private let backgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.gray.withAlphaComponent(0.3)
+        return view
+    }()
    
     // 상단 장바구니 요약 View
     private let summaryView: UIView = {
         let view = UIView()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = ThemeColors.red
+        view.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner] // 부분만 cornerRadius
+        view.layer.cornerRadius = 20
         return view
     }()
     
@@ -26,7 +35,8 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     private let closeButton: UIButton = {
         let button = UIButton()
         button.setTitle("닫기 X", for: .normal)
-        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = ThemeFonts.p
+        button.setTitleColor(ThemeColors.white, for: .normal)
         return button
     }()
     
@@ -34,7 +44,8 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     lazy var countTotalItemLabel: UILabel = {
         let label = UILabel()
         label.text = "장바구니 : \(totalCount) 개"
-        label.textColor = .white
+        label.textColor = ThemeColors.white
+        label.font = ThemeFonts.bold
         label.textAlignment = .left
         return label
     }()
@@ -43,7 +54,8 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     lazy var totalItemPriceLabel: UILabel = {
         let label = UILabel()
         label.text = "합계금액 : \(totalPrice) 원"
-        label.textColor = .white
+        label.textColor = ThemeColors.white
+        label.font = ThemeFonts.bold
         label.textAlignment = .right
         return label
     }()
@@ -51,7 +63,6 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     // 장바구니 목록을 보여주는 TableView
     private let tableView: UITableView = {
         let tableView = UITableView()
-        tableView.backgroundColor = .orange     // 테스트
         return tableView
     }()
     
@@ -70,15 +81,24 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     //MARK: - 레이아웃
     
     private func setupView() {
-        addSubview(summaryView)
+        addSubview(backgroundView)
         summaryView.addSubview(closeButton)
         summaryView.addSubview(countTotalItemLabel)
         summaryView.addSubview(totalItemPriceLabel)
-        addSubview(tableView)
+        backgroundView.addSubview(summaryView)
+        backgroundView.addSubview(tableView)
+        
+        // background Layout
+        backgroundView.snp.makeConstraints {
+            $0.top.equalToSuperview()
+            $0.bottom.equalToSuperview()
+            $0.leading.equalToSuperview()
+            $0.trailing.equalToSuperview()
+        }
         
         // 상단바 Layout
         summaryView.snp.makeConstraints {
-            $0.top.equalToSuperview()
+            $0.top.equalToSuperview().inset(200)
             $0.width.equalToSuperview()
             $0.height.equalTo(80)
         }
@@ -86,20 +106,20 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
         // 상단바 닫기버튼 Layout
         closeButton.snp.makeConstraints {
             $0.top.equalTo(summaryView.snp.top)
-            $0.trailing.equalTo(summaryView.snp.trailing).inset(10)
+            $0.trailing.equalTo(summaryView.snp.trailing).inset(ThemeNumbers.paddingSmall)
             $0.width.height.equalTo(50)
         }
         
         // 상단바 장바구니 갯수 Layout
         countTotalItemLabel.snp.makeConstraints {
-            $0.leading.equalTo(summaryView.snp.leading).inset(10)
+            $0.leading.equalTo(summaryView.snp.leading).inset(ThemeNumbers.paddingSmall)
             $0.bottom.equalTo(summaryView.snp.bottom)
             $0.height.equalTo(50)
         }
         
         // 상단바 합계금액 Layout
         totalItemPriceLabel.snp.makeConstraints {
-            $0.trailing.equalTo(summaryView.snp.trailing).inset(10)
+            $0.trailing.equalTo(summaryView.snp.trailing).inset(ThemeNumbers.paddingSmall)
             $0.bottom.equalTo(summaryView.snp.bottom)
             $0.height.equalTo(50)
         }
@@ -108,7 +128,7 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
         tableView.snp.makeConstraints {
             $0.width.equalToSuperview()
             $0.top.equalTo(summaryView.snp.bottom)
-            $0.bottom.equalToSuperview()
+            $0.bottom.equalToSuperview().inset(200)
         }
     }
      
@@ -131,6 +151,8 @@ class CartView: UIView, UITableViewDelegate, UITableViewDataSource{
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "CartItemButtonCell", for: indexPath) as? CartItemButtonCell else { return UITableViewCell() }
+        
+        cell.selectionStyle = .none // 셀 선택 색상 제거
         return cell
     }
 }
