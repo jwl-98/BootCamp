6bec0e72895f53ad65680660cbd8a4398354dcfb
이재영 <ldc1000@naver.com>
2024-11-28 18:59:42 +0900

    [Feat] ThemeMode
    테마 열거형

---
+++ nbc-popcon/nbc-popcon/Resources/ThemeManager/ThemeMode.swift
@@ -0,0 +1,14 @@
+//
+//  ThemeMode.swift
+//  nbc-popcon
+//
+//  Created by t2023-m0072 on 11/28/24.
+//
+
+import Foundation
+
+enum ThemeMode: String, CaseIterable {
+    case auto = "자동"
+    case light = "라이트 모드"
+    case dark = "다크모드"
+}
